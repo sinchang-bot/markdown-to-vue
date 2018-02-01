695149de7aad3fb32b6f426583daa4bcab1b2a0f
sinchang <sinchangwen@gmail.com>
2018-02-01 19:19:28 +0800

    update config

--- poi.config.js
+++ poi.config.js
@@ -6,9 +6,14 @@ module.exports = {
   extendWebpack(config) {
     config.module.rule('markdown')
       .test(/\.md$/)
+      .exclude
+        .add(/(node_modules|bower_components)/)
+        .end()
+      .use('vue')
+        .loader('vue-loader')
+        .end()
       .use('markdown-to-vue')
         .loader('markdown-to-vue-loader')
         .options({})
-        .end()
   }
 }
--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -2,3 +2,8 @@ declare module "*.vue" {
   import Vue from "vue"
   export default Vue
 }
+
+declare module "*.md" {
+  import Vue from "vue"
+  export default Vue
+}
